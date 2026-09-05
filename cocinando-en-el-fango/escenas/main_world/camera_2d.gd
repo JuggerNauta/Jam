00535376bae87da2ffdc0135fbe2bac22f54aba5
Andres <aporrasmdz@gmail.com>
2026-09-05 15:38:52 -0500

    fix: camera issue

--- cocinando-en-el-fango/escenas/main_world/camera_2d.gd
+++ cocinando-en-el-fango/escenas/main_world/camera_2d.gd
@@ -14,26 +14,28 @@ func _ready() -> void:
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	var input_vector = Input.get_vector("izquierda", "derecha", "arriba", "abajo")
 	calculate_velocity(input_vector)
 	update_global_position()
-	#apply_camera_limits()
+	apply_camera_limits()
 	
 
 func apply_camera_limits():
 	var tilemap_info = get_tilemap_info()
-	var _level_size = Vector2i(tilemap_info.tile_size * tilemap_info.size)
+	var level_size = Vector2i(tilemap_info.tile_size * tilemap_info.size)
 	#set_limit(SIDE_LEFT, 0)
 	#set_limit(SIDE_TOP, -level_size.y/2)
 	#set_limit(SIDE_RIGHT, level_size.x)
 	#set_limit(SIDE_BOTTOM, level_size.y/2)
 	#
 func update_global_position():
-	var _delta = get_process_delta_time()
-	var viewport_size = Vector2i(get_viewport_rect().size)
-	var current_cell = Vector2i(player.global_position) / Vector2i(viewport_size)
+	var delta = get_process_delta_time()
+	var viewport_size: Vector2i  = get_viewport_rect().size
+	var current_cell = Vector2i(player.global_position) / viewport_size
 	
+	print("Player POS",player.global_position, "VP SIZE", viewport_size)
+	print("LA CELDA ACTUAL ES", current_cell)
 	global_position = current_cell * viewport_size
 	#global_position += lerp(
 		#velocity,
