7858293c484ffec751ae065ed5c92ed7dff3f99c
Andres <aporrasmdz@gmail.com>
2026-09-05 19:15:15 -0500

    feat: default paths, spawner areas

--- cocinando-en-el-fango/escenas/main_world/enemy_handler.gd
+++ cocinando-en-el-fango/escenas/main_world/enemy_handler.gd
@@ -1,18 +1,8 @@
 
-extends Area2D
+extends Node2D
 
-@export var max_enemies: int = 5
-@onready var spawn_area: CollisionShape2D = $CollisionShape2D
-
-var current_enemies: int = 0
 
 	
-func get_random_position():
-	var rect = spawn_area.shape as RectangleShape2D
-	var size = rect.size / 2.0 
-	var x = randf_range(-size.x, size.x)
-	var y = randf_range(-size.y, size.y)
-	return Vector2(x, y)	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
