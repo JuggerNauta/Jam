7211890fc482764678cf5a0bdd26f67f9b452069
JoelVeloz <joevpazm@espol.edu.ec>
2026-09-05 21:39:34 -0500

    FEAT: INV SELECTCIONABLE

---
+++ cocinando-en-el-fango/scrips/selector_slot.gd
@@ -0,0 +1,15 @@
+extends Node
+
+signal cambio(indice: int)
+
+var indice := 0
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not (event is InputEventKey and event.pressed and not event.echo):
+		return
+
+	if event.keycode >= KEY_1 and event.keycode <= KEY_6:
+		indice = event.keycode - KEY_1
+		cambio.emit(indice)
+	elif event.keycode == KEY_E:
+		Inventario.soltar_objeto(indice)
