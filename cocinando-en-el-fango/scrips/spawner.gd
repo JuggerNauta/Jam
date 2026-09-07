0e495c896c4e4b56b8280c9d558ed2ae8e142159
Andres <aporrasmdz@gmail.com>
2026-09-06 23:27:32 -0500

    fix: loot table

--- cocinando-en-el-fango/scrips/spawner.gd
+++ cocinando-en-el-fango/scrips/spawner.gd
@@ -7,11 +7,8 @@ extends Node2D
 
 var total_enemigos = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass 
 func _process(_delta: float) -> void:
 	pass
 
