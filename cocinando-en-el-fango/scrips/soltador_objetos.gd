9e5f7491c51f032a927d51bc3b166db92686b9bf
Andres <aporrasmdz@gmail.com>
2026-09-07 01:02:17 -0500

    fix: player dir

--- cocinando-en-el-fango/scrips/soltador_objetos.gd
+++ cocinando-en-el-fango/scrips/soltador_objetos.gd
@@ -6,11 +6,11 @@ func _ready() -> void:
 	Inventario.objeto_soltado.connect(_on_objeto_soltado)
 
 func _on_objeto_soltado(_indice: int, objeto: ObjetoData) -> void:
-	var jugador := get_tree().get_first_node_in_group("jugador")
+	var jugador := get_tree().get_first_node_in_group("jugador") as Player
 	var instancia := OBJETO_RECOGIBLE.instantiate()
 	var sprite = instancia.get_node("ItemSprite") as Sprite2D
 	
 	sprite.texture = objeto.sprite
 	instancia.stack = ItemStack.new(objeto, 1)
-	instancia.global_position = jugador.global_position + Vector2(0, 100)
+	instancia.global_position = jugador.global_position + jugador.ultima_direccion * 32
 	get_tree().current_scene.add_child(instancia)
